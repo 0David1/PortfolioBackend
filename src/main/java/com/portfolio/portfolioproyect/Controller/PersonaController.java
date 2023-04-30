7865368861e1ee93ebe78c661f55eb99943a94c7
David <enzobautista1000@gmail.com>
2023-04-30 17:11:39 -0300

    save8

--- src/main/java/com/portfolio/portfolioproyect/Controller/PersonaController.java
+++ src/main/java/com/portfolio/portfolioproyect/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"http://localhost:4200, https://enzo-bautista-01.web.app"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://enzo-bautista-01.web.app"})
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
