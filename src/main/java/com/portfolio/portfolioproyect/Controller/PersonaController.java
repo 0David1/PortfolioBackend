bc55e6a9c2edb640d3b7cd0a3794a83b4a39c35f
David <enzobautista1000@gmail.com>
2023-05-01 08:47:33 -0300

    save13

--- src/main/java/com/portfolio/portfolioproyect/Controller/PersonaController.java
+++ src/main/java/com/portfolio/portfolioproyect/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"http://localhost:4200", "https://enzo-bautista-01.web.app"})
+@CrossOrigin(origins = "*")
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
--- src/main/java/com/portfolio/portfolioproyect/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/portfolioproyect/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200", "https://enzo-bautista-01.web.app"})
+@CrossOrigin(origins = "*")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
